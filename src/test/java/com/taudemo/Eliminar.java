743c41b96e074b0e5bc6567694dccf6d77205ab1
ahuaracab <ahuaracab@gmail.com>
2020-03-08 00:02:30 -0500

    agregar clase para eliminar

---
+++ src/test/java/com/taudemo/Eliminar.java
@@ -0,0 +1,22 @@
+package com.taudemo;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Eliminar {
+
+    @Test
+    public void firstAssertion() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test
+    public void secondAssertion() {
+        Assert.assertEquals(45, 45);
+    }
+
+    @Test
+    public void thirdAssertion() {
+        Assert.assertEquals(34, 34);
+    }
+}
